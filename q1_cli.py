63c7565926330517d62fdb98f226ea7670cfecbe
Mohammad Amin Safari <safarimoamin@gmail.com>
2023-12-15 12:10:23 +0330

    Add Files

---
+++ q1_cli.py
@@ -0,0 +1,82 @@
+import socket
+import threading
+
+def see_all_clients():
+    try:
+        server_address = ('127.0.0.1', 11521)
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.sendto('see clients'.encode('utf-8'), server_address)
+        response, addr = udp_socket.recvfrom(1024)
+        print(f"Connected clients: {response.decode('utf-8')}")
+        udp_socket.close()
+    except Exception as e:
+        print(f"Error seeing clients: {str(e)}")
+
+def send_messages(client, action):
+    receiver = 'public'
+    if action == '1':
+        receiver = input('Enter receiver username: ')
+    message = input('Enter message: ')
+    client.send(f'{receiver}\n\0\n{message}'.encode('utf-8'))
+    print(client.recv(1024).decode('utf-8'))
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            response = client_socket.recv(1024)
+            if not response:
+                break
+            print(f"{response.decode('utf-8')}")
+        except Exception as e:
+            print(f"Error receiving message: {str(e)}")
+            break
+
+def init_client(client, log_or_sign):
+    while True:
+        username = input("Enter Username (type 'exit' to quit): ")
+        if username.lower() == 'exit':
+            return False
+        password = input("Enter Password: ")
+        client.send(f'{log_or_sign}\n\0\n{username}\n\0\n{password}'.encode('utf-8'))
+        response = client.recv(1024)
+        if response.decode('utf-8') == 'Authenticated':
+            return True
+        print(response.decode('utf-8'))
+
+def connect_to_server(server_address, log_or_sign):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(server_address)
+        print("Connected to server")
+        if not init_client(client, log_or_sign):
+            client.close()
+            return
+        receive_thread = threading.Thread(target=receive_messages, args=(client,))
+        receive_thread.start()
+        while True:
+            action = input('1. Send private message\n2. Send public message\n3. See all clients\nEnter Num:\n')
+            if action == '1' or action == '2':
+                send_messages(client, action)
+            elif action == '3':
+                see_all_clients()
+            else:
+                print('Invalid action')
+    except Exception as e:
+        print(f"Error while connecting to server: {str(e)}")
+
+def main():
+    while True:
+        f_action = input('1. Log in\n2. Sign up\n3. See all clients\n4. Exit\nEnter Num:\n')
+        if f_action == '1':
+            connect_to_server(('127.0.0.1', 11520), 'login')
+        elif f_action == '2':
+            connect_to_server(('127.0.0.1', 11520), 'signup')
+        elif f_action == '3':
+            see_all_clients()
+        elif f_action == '4':
+            break
+        else:
+            print('Invalid action')
+
+if __name__ == "__main__":
+    main()
---
+++ q1_ser.py
@@ -0,0 +1,101 @@
+import socket
+import threading
+from cryptography.fernet import Fernet
+
+username_password = {}
+username_socket = {}
+key = Fernet.generate_key()
+fernet = Fernet(key)
+
+def password_check(username, password, log_or_sign):
+    print(f'Received Password for {username}: {password}')
+    if log_or_sign == 'login':
+        if password == fernet.decrypt(username_password[username]).decode('utf-8'):
+            return 'Authenticated'
+        else:
+            return 'Wrong Password,\nPlease try again:\n'
+    elif log_or_sign == 'signup':
+        return 'Authenticated'
+
+def username_check(username, log_or_sign):
+    if log_or_sign == 'login':
+        if username not in username_password.keys():
+            return 'Username not found,\nPlease try again:\n'
+        else:
+            return 'Client Initialized'
+    elif log_or_sign == 'signup':
+        if username == 'public':
+            return 'You cannot use this username\nPlease try again:\n'
+        elif username in username_password.keys():
+            return 'Username already taken,\nPlease try again:\n'
+        else:
+            return 'Client Initialized'
+
+def init_client(client_socket):
+    username = ''
+    while True:
+        data = client_socket.recv(1024)
+        log_or_sign, username, password = data.decode('utf-8').split('\n\0\n', 2)
+        print(f'{log_or_sign} for {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}')
+        print(f'Received Username for {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}: {username}')
+
+        response = username_check(username, log_or_sign)
+        if response == 'Client Initialized':
+            response = password_check(username, password, log_or_sign)
+            if response == 'Authenticated':
+                username_password.update({username: fernet.encrypt(password.encode('utf-8'))})
+                username_socket.update({username: client_socket})
+
+        client_socket.send(response.encode('utf-8'))
+        if response == 'Authenticated':
+            break
+
+    print(f'Client {username} Initialized')
+
+def handle_client(client_socket):
+    init_client(client_socket)
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        decoded_data = data.decode('utf-8')
+        dest_name, message = decoded_data.split('\n\0\n', 1)
+        response = f'Message sent to {dest_name}'
+        if dest_name == 'public':
+            for client in username_socket.values():
+                client.send(f'Public message from {list(username_socket.keys())[list(username_socket.values()).index(client_socket)]}:\n{message}'.encode('utf-8'))
+        elif dest_name in username_socket.keys():
+            username_socket[dest_name].send(f'Private message from {list(username_socket.keys())[list(username_socket.values()).index(client_socket)]}:\n{message}'.encode('utf-8'))
+        else:
+            response = 'Invalid destination'
+        client_socket.send(response.encode('utf-8'))
+    client_socket.close()
+
+def udp_see_clients():
+    server_address = ('127.0.0.1', 11521)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(server_address)
+    while True:
+        data, addr = udp_socket.recvfrom(1024)
+        if not data:
+            break
+        udp_socket.sendto(str(list(username_password.keys())).encode('utf-8'), addr)
+
+def main():
+    try:
+        udp_thread = threading.Thread(target=udp_see_clients)
+        udp_thread.start()
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 11520))
+        server.listen(5)
+        print('*Server listening messages on port 11520*')
+        while True:
+            client, addr = server.accept()
+            print(f'*Accepted connection from {addr[0]}:{addr[1]}*')
+            client_handler = threading.Thread(target=handle_client, args=(client,))
+            client_handler.start()
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+if __name__ == '__main__':
+    main()
