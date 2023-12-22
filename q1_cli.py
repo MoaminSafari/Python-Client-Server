94dd0839338c99f58428e90815b88fde140099ef
Mohammad Amin Safari <safarimoamin@gmail.com>
2023-12-22 22:33:23 +0330

    auto format files

--- q1_cli.py
+++ q1_cli.py
@@ -2,6 +2,7 @@ import socket
 import threading
 from datetime import datetime
 
+
 class Client:
     def __init__(self):
         self.isLoggedIn = False
@@ -13,7 +14,8 @@ class Client:
         try:
             server_address = ('127.0.0.1', 11521)
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-                udp_socket.sendto('see clients'.encode('utf-8'), server_address)
+                udp_socket.sendto('see clients'.encode(
+                    'utf-8'), server_address)
                 response, addr = udp_socket.recvfrom(1024)
                 print(f"Connected clients: {response.decode('utf-8')}")
         except Exception as e:
@@ -22,17 +24,20 @@ class Client:
     def send_messages(self, action):
         receiver = 'public'
         if action == '1':
-            receiver = input('Enter receiver username or group name (start with @): ')
+            receiver = input(
+                'Enter receiver username or group name (start group name with @): ')
         message = input('Enter message: ')
         now = datetime.now()
         current_time = now.strftime("%H:%M")
         with self.lock:
             if receiver.startswith('@'):
                 holder = receiver[1:]
-                self.server.send(f'@group:{holder}\n\0\n{message}\n\t{current_time}'.encode('utf-8'))
+                self.server.send(
+                    f'@group:{holder}\n\0\n{message}\n\t{current_time}'.encode('utf-8'))
             else:
-                self.server.send(f'{receiver}\n\0\n{message}\n\t{current_time}'.encode('utf-8'))
-                
+                self.server.send(
+                    f'{receiver}\n\0\n{message}\n\t{current_time}'.encode('utf-8'))
+
     def receive_messages(self, client_socket):
         while True:
             try:
@@ -45,7 +50,7 @@ class Client:
                 # elif response.decode('utf-8').startswith('@status:'):
                     # with self.lock:
                         # _,self.status = response.split(':')
-                        # response = f'status updated to {self.status}' 
+                        # response = f'status updated to {self.status}'
                 print(f"{response.decode('utf-8')}")
             except Exception as e:
                 print(f"Error receiving message: {str(e)}")
@@ -55,13 +60,14 @@ class Client:
         if self.isLoggedIn or self.server is None:
             return
         username = input("Enter Username (type 'exit' to quit): ")
-        if username=='exit':
+        if username == 'exit':
             return False
         password = input("Enter Password: ")
         with self.lock:
             if self.server is not None:  # Check if the server socket is still valid
-                print("checking the user name with server")
-                self.server.send(f'{log_or_sign}\n\0\n{username}\n\0\n{password}'.encode('utf-8'))
+                print("Checking the user name with server")
+                self.server.send(
+                    f'{log_or_sign}\n\0\n{username}\n\0\n{password}'.encode('utf-8'))
         return True
 
     def connect_to_server_port(self, server_address):
@@ -70,19 +76,18 @@ class Client:
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server.connect(server_address)
             print("Connected to server")
-            receive_thread = threading.Thread(target=self.receive_messages, args=(server,))
+            receive_thread = threading.Thread(
+                target=self.receive_messages, args=(server,))
             receive_thread.daemon = True
             receive_thread.start()
             self.server = server
         except Exception as e:
             print(f"Error while connecting to server: {str(e)}")
 
-    
     def changeStatus(self):
-        message = input('set your status (busy, online, offline ,...): ')
+        message = input('Set your status (busy, online, offline): ')
         with self.lock:
             self.server.send(f'@status\n\0\n{message}'.encode('utf-8'))
- 
 
     def connect_to_server(self):
         try:
@@ -90,7 +95,8 @@ class Client:
                 return
 
             while True:
-                action = input(f'{self.status}\n0.Refresh\n1. Send private message\n2. Send public message\n3. See all clients\n4. Group actions\n5. status\n6. Exit\nEnter Num:\n')
+                action = input(
+                    f'{self.status}\n0.Refresh\n1. Send private message\n2. Send public message\n3. See all clients\n4. Group actions\n5. status\n6. Exit\nEnter Num:\n')
                 if action == '0':
                     continue
                 elif action == '1' or action == '2':
@@ -110,7 +116,8 @@ class Client:
             print(f"Error while connecting to server: {str(e)}")
 
     def create_or_join_group(self):
-        group_action = input('1. Create a group\n2. Join a group\n3.exit \nEnter Num:\n')
+        group_action = input(
+            '1. Create a group\n2. Join a group\n3. Exit \nEnter Num:\n')
         if group_action == '1':
             group_name = input('Enter group name: ')
             with self.lock:
@@ -124,10 +131,11 @@ class Client:
         #     memeber_name = input('enter the member name: ')
         #     with self.lock:
         #         self.server.send(f'@add:{group_name}\n\0\n{memeber_name}'.encode('utf-8'))
-        elif  group_action == '3':
-            pass 
+        elif group_action == '3':
+            pass
         else:
             print('Invalid group action')
+
 
 def main():
     client = Client()
@@ -137,8 +145,9 @@ def main():
         if client.isLoggedIn:
             client.connect_to_server()
             break
-            
-        f_action = input('0.Refresh\n1. Log in\n2. Sign up\n3. See all clients\n4. Exit\nEnter Num:\n')
+
+        f_action = input(
+            '0.Refresh\n1. Log in\n2. Sign up\n3. See all clients\n4. Exit\nEnter Num:\n')
         if f_action == '0':
             continue
         if f_action == '1':
@@ -153,6 +162,7 @@ def main():
             break
         else:
             print('Invalid action')
+
 
 if __name__ == "__main__":
     main()
--- q1_ser.py
+++ q1_ser.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 
 username_password = {}
 username_socket = {}
-username_messages={}
+username_messages = {}
 key = Fernet.generate_key()
 fernet = Fernet(key)
 groups = {}
@@ -20,6 +20,7 @@ def password_check(username, password, log_or_sign):
             return 'Wrong Password,\nPlease try again:\n'
     elif log_or_sign == 'signup':
         return 'Authenticated'
+
 
 def username_check(username, log_or_sign):
     if log_or_sign == 'login':
@@ -43,7 +44,8 @@ def create_group(username, group_name):
     else:
         return f'Group {group_name} already exists'
 
-def join_group(username, group_name, requesting_user,isPublic):
+
+def join_group(username, group_name, requesting_user, isPublic):
     if group_name in groups:
         if (username in groups[group_name]) or isPublic:
             groups[group_name].append(username_socket[username])
@@ -53,11 +55,13 @@ def join_group(username, group_name, requesting_user,isPublic):
     else:
         return f'Group {group_name} not found'
 
+
 def send_group_message(username, group_name, message):
     if group_name in groups:
         if username_socket[username] in groups[group_name]:
             for member_socket in groups[group_name]:
-                member_socket.send(f'Group message from {username} in {group_name}:\n{message}'.encode('utf-8'))
+                member_socket.send(
+                    f'Group message from {username} in {group_name}:\n{message}'.encode('utf-8'))
             return f'Group message sent to {group_name}'
         else:
             return f'you are not allowed to send message in this Group {group_name} '
@@ -69,29 +73,35 @@ def init_client(client_socket):
     username = ''
     while True:
         data = client_socket.recv(1024)
-        log_or_sign, username, password = data.decode('utf-8').split('\n\0\n', 2)
-        print(f'{log_or_sign} for {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}')
-        print(f'Received Username for {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}: {username}')
+        log_or_sign, username, password = data.decode(
+            'utf-8').split('\n\0\n', 2)
+        print(
+            f'{log_or_sign} for {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}')
+        print(
+            f'Received Username for {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}: {username}')
 
         response = username_check(username, log_or_sign)
         if response == 'Client Initialized':
             response = password_check(username, password, log_or_sign)
             if response == 'Authenticated':
-                username_password.update({username: fernet.encrypt(password.encode('utf-8'))})
+                username_password.update(
+                    {username: fernet.encrypt(password.encode('utf-8'))})
                 username_socket.update({username: client_socket})
                 status.update({username: 'Online'})
         client_socket.send(response.encode('utf-8'))
         if response == 'Authenticated':
-            if list(username_socket.keys())[list(username_socket.values()).index(client_socket)] in username_messages :
+            if list(username_socket.keys())[list(username_socket.values()).index(client_socket)] in username_messages:
                 client_socket.send(f'Older messages:\n'.encode('utf-8'))
                 for message in username_messages[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]]:
                     client_socket.send(f'{message}\n'.encode('utf-8'))
             else:
                 client_socket.send(f'There are no messages\n'.encode('utf-8'))
-                username_messages[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]]=[]
+                username_messages[list(username_socket.keys())[list(
+                    username_socket.values()).index(client_socket)]] = []
             break
 
     print(f'Client {username} Initialized')
+
 
 def handle_client(client_socket):
     init_client(client_socket)
@@ -105,37 +115,49 @@ def handle_client(client_socket):
         if dest_name == '@status':
             new_status = message.strip()
             print(new_status)
-            status[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]] = new_status
+            status[list(username_socket.keys())[
+                list(username_socket.values()).index(client_socket)]] = new_status
             response = f'@status:{new_status}'
         elif dest_name.startswith('@'):
             command, group_name = dest_name.split(':')
             if command == '@create':
-                response = create_group(list(username_socket.keys())[list(username_socket.values()).index(client_socket)], group_name)
+                response = create_group(list(username_socket.keys())[list(
+                    username_socket.values()).index(client_socket)], group_name)
             elif command == '@join':
-                response = join_group(list(username_socket.keys())[list(username_socket.values()).index(client_socket)], group_name,list(username_socket.keys())[list(username_socket.values()).index(client_socket)],True)
+                response = join_group(list(username_socket.keys())[list(username_socket.values()).index(
+                    client_socket)], group_name, list(username_socket.keys())[list(username_socket.values()).index(client_socket)], True)
             elif command == '@group':
-                response = send_group_message(list(username_socket.keys())[list(username_socket.values()).index(client_socket)], group_name, message)
+                response = send_group_message(list(username_socket.keys())[list(
+                    username_socket.values()).index(client_socket)], group_name, message)
             elif command == '@add':
-                response = join_group(list(username_socket.keys())[list(username_socket.values()).index(client_socket)], group_name,message,True)
+                response = join_group(list(username_socket.keys())[list(
+                    username_socket.values()).index(client_socket)], group_name, message, True)
             else:
                 response = 'Invalid group command'
         elif dest_name == 'public':
             for client in username_socket.values():
-                clientstatus = status[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]]
-                if  clientstatus!='busy' and clientstatus != 'offline':
-                    username_messages[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]].append(f'From: {list(username_socket.values()).index(client_socket)}\nTo: Public\n{message}')
-                    client.send(f'Public message from {list(username_socket.values()).index(client_socket)}:\n{message}'.encode('utf-8'))
+                clientstatus = status[list(username_socket.keys())[list(
+                    username_socket.values()).index(client_socket)]]
+                if clientstatus != 'busy' and clientstatus != 'offline':
+                    username_messages[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]].append(
+                        f'From: {list(username_socket.values()).index(client_socket)}\nTo: Public\n{message}')
+                    client.send(
+                        f'Public message from {list(username_socket.values()).index(client_socket)}:\n{message}'.encode('utf-8'))
         elif dest_name in username_socket.keys():
-            clientstatus = status[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]]
-            if  clientstatus!='busy' and clientstatus != 'offline':
-                username_messages[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]].append(f'From: {list(username_socket.values()).index(client_socket)}\nTo: {dest_name}\n{message}')
-                username_socket[dest_name].send(f'Private message from {list(username_socket.keys())[list(username_socket.values()).index(client_socket)]}:\n{message}'.encode('utf-8'))
+            clientstatus = status[list(username_socket.keys())[list(
+                username_socket.values()).index(client_socket)]]
+            if clientstatus != 'busy' and clientstatus != 'offline':
+                username_messages[list(username_socket.keys())[list(username_socket.values()).index(client_socket)]].append(
+                    f'From: {list(username_socket.values()).index(client_socket)}\nTo: {dest_name}\n{message}')
+                username_socket[dest_name].send(
+                    f'Private message from {list(username_socket.keys())[list(username_socket.values()).index(client_socket)]}:\n{message}'.encode('utf-8'))
             else:
                 response = f'{dest_name} this user is in {clientstatus} status'
         else:
             response = 'Invalid destination'
         client_socket.send(response.encode('utf-8'))
     client_socket.close()
+
 
 def udp_see_clients():
     server_address = ('127.0.0.1', 11521)
@@ -145,7 +167,9 @@ def udp_see_clients():
         data, addr = udp_socket.recvfrom(1024)
         if not data:
             break
-        udp_socket.sendto(str(list(username_password.keys())).encode('utf-8'), addr)
+        udp_socket.sendto(
+            str(list(username_password.keys())).encode('utf-8'), addr)
+
 
 def main():
     try:
@@ -158,10 +182,12 @@ def main():
         while True:
             client, addr = server.accept()
             print(f'*Accepted connection from {addr[0]}:{addr[1]}*')
-            client_handler = threading.Thread(target=handle_client, args=(client,))
+            client_handler = threading.Thread(
+                target=handle_client, args=(client,))
             client_handler.start()
     except Exception as e:
         print(f"Error: {str(e)}")
+
 
 if __name__ == '__main__':
     main()
